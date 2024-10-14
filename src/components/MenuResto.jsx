0ff2a21b172c70c6cf24a490899122027140db9f
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-14 09:43:12 +0800

    Looping

--- src/components/MenuResto.jsx
+++ src/components/MenuResto.jsx
@@ -3,24 +3,23 @@ import '../style/MenuResto.css'
 function MenuResto() {
 
 
-  const isMakanan = true
+const menu = [
+ {nama: "nasi ayam", harga:"1.324"},
+ {nama: "es teh", harga:"3e03"},
+ {nama: "jamu", harga: "32e324"}
+]
 
   return (
-    // ketika mengunakan lebih satu div maka mesti dibungkus dulu mengunakan fragment <></>
-
-     
-
-    <div className="menu-item">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas omnis quos eum, illum harum praesentium?</p>
-        {
-            // apakah is makanan true kalau tidak tipe minuman
-            // jika true maka dia masuk setelah tanda tanya 
-            // (isMakanan == true) ? (<div>tipe: makanan</div>):(<div>tipe: minuman</div>)
-
-            (isMakanan == true) && (<div>tipe: makanan</div>)
-        }
-    </div>
-    
+    <>
+    {
+          menu.map((item, index)=> (
+            <div key={index} className='menu-item'>
+              <div><b>Nama {item.nama}</b>  Harga: {item.harga}</div>
+            </div>
+          ))
+    }    
+    </>
+ 
 
     
   )
