3ac5f15d942b3a7f00ce8ff010286db5534805bd
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-14 09:15:56 +0800

    styling component

--- src/components/MenuResto.jsx
+++ src/components/MenuResto.jsx
@@ -1,13 +1,16 @@
-
-
+import '../style/MenuResto.css'
 
 function MenuResto() {
   return (
     // ketika mengunakan lebih satu div maka mesti dibungkus dulu mengunakan fragment <></>
-    <>
-    <div><b>Nama Menu </b>MenuResto</div>
-    <div><b>Nama Menu </b>MenuResto</div>
-    </>
+    <div>
+
+    <div className="menu-item">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas omnis quos eum, illum harum praesentium?</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    
+    </div>
     
   )
 }
