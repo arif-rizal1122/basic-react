c0af60947a311b59dc42b2c7c6ae93f93207d99e
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-14 09:25:23 +0800

    conditional rendering

--- src/components/MenuResto.jsx
+++ src/components/MenuResto.jsx
@@ -1,16 +1,27 @@
 import '../style/MenuResto.css'
 
 function MenuResto() {
+
+
+  const isMakanan = true
+
   return (
     // ketika mengunakan lebih satu div maka mesti dibungkus dulu mengunakan fragment <></>
-    <div>
+
+     
 
     <div className="menu-item">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas omnis quos eum, illum harum praesentium?</p>
-        <p>Lorem ipsum dolor sit amet.</p>
+        {
+            // apakah is makanan true kalau tidak tipe minuman
+            // jika true maka dia masuk setelah tanda tanya 
+            // (isMakanan == true) ? (<div>tipe: makanan</div>):(<div>tipe: minuman</div>)
+
+            (isMakanan == true) && (<div>tipe: makanan</div>)
+        }
     </div>
     
-    </div>
+
     
   )
 }
