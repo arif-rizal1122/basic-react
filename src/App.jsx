aaf19dda5b30d99a89599dd6784f1ef022e421c3
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-14 17:04:00 +0800

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,29 @@
-import MenuResto from "./components/MenuResto"
-import Header from "./components/Header"
-import Mood from "./components/Mood"
-import Button from "./components/Button"
-import { useCallback, useMemo, useState } from "react"
-import Child from "./components/Child"
-
+import React, { useRef } from 'react'
 
 function App() {
 
+  const pesan = useRef(null)
+  const displayPesan = useRef(null)
 
-  // function click() {
-  //    console.log('dari app')
-  // }
-
-  const [likeCount, setLikeCount] = useState(0)
-  const [subsCount, setSubsCount] = useState(0)
-  const [name, setName] = useState("arif_rizal")
-
-
-
-  // useCallback(fungsi(), [dependencies])
-  const handleName = useCallback(() => {
-    let channelName = ''
-
-    if (name == 'arif_rizal') {
-        channelName = "wpu"
-    } else {
-        channelName = 'rumah saya'
-    }
-    setName(channelName)
-    console.log(`run in ${name}`)
-  }, [name])
+  function clickHandle() {
+    console.log(pesan.current.value)
+    displayPesan.current.innerHTML = pesan.current.value
+    displayPesan.current.style.padding="5px";
+    displayPesan.current.style.backgroundColor="#ffff";
+    displayPesan.current.style.color="black";
+  }
 
   return (
     <>
-      <Header />
-      <MenuResto />
-      <Mood hari="selasa" mood="sad"/>
-      {/* <Button click={click}/> */}
-      <Button />
-
-      <p>
-         <button onClick={()=>setLikeCount(likeCount+1)}>{likeCount} Like </button>
-      </p>
-
-      <p>
-         <button onClick={()=>setSubsCount(subsCount+1)}>({subsCount}) Subscribe </button>
-      </p>
-
-      <Child name={name} aksi={handleName}/>
       
+      <div>
+        <input type="text" ref={pesan} placeholder='send your image'/>
+      </div>
+      <div>
+        <button onClick={clickHandle}>Click Me</button>
+      </div>
+      <div ref={displayPesan}></div>
     </>
-    
   )
 }
 
