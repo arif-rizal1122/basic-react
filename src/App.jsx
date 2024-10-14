dcb72e96261202f7bb470ed266ece44c87dcdf22
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-14 15:55:34 +0800

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,41 @@ import MenuResto from "./components/MenuResto"
 import Header from "./components/Header"
 import Mood from "./components/Mood"
 import Button from "./components/Button"
+import { useMemo, useState } from "react"
 
 
 function App() {
 
-  function click() {
-     console.log('dari app')
+
+  // function click() {
+  //    console.log('dari app')
+  // }
+
+  const [likeCount, setLikeCount] = useState(0)
+  const [subsCount, setSubsCount] = useState(0)
+
+  function pesanLike() {
+      console.log(`pesan like rendered`)
+      return (likeCount < 10) ? 'like kurang dari 10' : 'alhamdulillah lebih dari 10'
   }
+  
+  const displayPesanLike = useMemo(()=>pesanLike(), [likeCount])
 
   return (
     <>
       <Header />
       <MenuResto />
       <Mood hari="selasa" mood="sad"/>
-      <Button click={click}/>
+      {/* <Button click={click}/> */}
+      <Button />
+
+      <p>
+         <button onClick={()=>setLikeCount(likeCount+1)}>{likeCount} Like </button> <b> {displayPesanLike}</b>
+      </p>
+
+      <p>
+         <button onClick={()=>setSubsCount(subsCount+1)}>({subsCount}) Subscribe </button>
+      </p>
     </>
     
   )
